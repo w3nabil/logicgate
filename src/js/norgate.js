3102ccdd7823704f8f24e52d2be21c0d9a5d2ab0
NABIL AHMED <124899245+w3nabil@users.noreply.github.com>
2023-11-10 23:44:54 +0600

    Updated norgate.js

--- src/js/norgate.js
+++ src/js/norgate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const nor1 = document.getElementById('nor1');
 const nor2 = document.getElementById('nor2');
 const norbulb = document.getElementById('norbulb');
@@ -33,42 +32,4 @@ function checkButtons_nor() {
     norbulb.src = "https://www.w3schools.com/js/pic_bulbon.gif";
   }
 }
-
-=======
-const nor1 = document.getElementById('nor1');
-const nor2 = document.getElementById('nor2');
-const norbulb = document.getElementById('norbulb');
-
-nor1.addEventListener('click', () => {
-  nor1.classList.toggle('nor-active');
-  checkButtons_nor();
-
-  if (nor1.classList.contains('nor-active')) {
-    nor1.textContent = "1";
-  } else {
-    nor1.textContent = "0";
-  }
-});
-
-nor2.addEventListener('click', () => {
-  nor2.classList.toggle('nor-active');
-  checkButtons_nor();
-
-  if (nor2.classList.contains('nor-active')) {
-    nor2.textContent = "1";
-  } else {
-    nor2.textContent = "0";
-  }
-});
-
-function checkButtons_nor() {
-    console.log("NOR GATE RUNNING WITHOUT ANY ERROR. PASS....")
-  if (nor1.classList.contains('nor-active') || nor2.classList.contains('nor-active')) {
-    norbulb.src = "https://www.w3schools.com/js/pic_bulboff.gif";
-  } else {
-    norbulb.src = "https://www.w3schools.com/js/pic_bulbon.gif";
-  }
-}
-
->>>>>>> 271e3179ae2bfecce85fd2190853c712740167be
 console.log("Successfully Loaded OR GATE... (5/7)")
