f533450893635be655f6deb28821ab53fb994268
NABIL AHMED <124899245+w3nabil@users.noreply.github.com>
2023-11-10 23:45:58 +0600

    Update orgate.js

--- src/js/orgate.js
+++ src/js/orgate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const or1 = document.getElementById('orgatebtn1');
 const or2 = document.getElementById('orgatebtn2');
 const orbulb = document.getElementById('orbulb');
@@ -33,42 +32,4 @@ function checkButtons_or() {
     orbulb.src = "https://www.w3schools.com/js/pic_bulboff.gif";
   }
 }
-
-=======
-const or1 = document.getElementById('orgatebtn1');
-const or2 = document.getElementById('orgatebtn2');
-const orbulb = document.getElementById('orbulb');
-
-or1.addEventListener('click', () => {
-  or1.classList.toggle('active');
-  checkButtons_or();
-
-  if (or1.classList.contains('active')) {
-    or1.textContent = "1";
-  } else {
-    or1.textContent = "0";
-  }
-});
-
-or2.addEventListener('click', () => {
-  or2.classList.toggle('active');
-  checkButtons_or();
-
-  if (or2.classList.contains('active')) {
-    or2.textContent = "1";
-  } else {
-    or2.textContent = "0";
-  }
-});
-
-function checkButtons_or() {
-    console.log("OR GATE RUNNING WITHOUT ANY ERROR. PASS....")
-  if (or1.classList.contains('active') || or2.classList.contains('active')) {
-    orbulb.src = "https://www.w3schools.com/js/pic_bulbon.gif";
-  } else {
-    orbulb.src = "https://www.w3schools.com/js/pic_bulboff.gif";
-  }
-}
-
->>>>>>> 271e3179ae2bfecce85fd2190853c712740167be
 console.log("Successfully Loaded OR GATE... (2/7)")
